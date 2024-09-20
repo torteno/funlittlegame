5aab306eb9fc13e583d944fc58575cb735b26e4f
Jack <jackwilson09@crescentschool.org>
2024-09-19 22:45:50 -0400

    Changes 1

--- src/myFrame.java
+++ src/myFrame.java
@@ -2,11 +2,13 @@ import javax.swing.*;
 import java.awt.Color;
 import java.awt.event.*;
 import javax.swing.JLabel;
+import java.awt.Rectangle;
 
 
 public class myFrame extends JFrame implements KeyListener {
 
     JLabel label;
+    JLabel obstacle;
 
     myFrame() {
         JFrame frame = new JFrame("My Frame!");
@@ -14,49 +16,59 @@ public class myFrame extends JFrame implements KeyListener {
         frame.setSize(1500,1000);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
+
+        obstacle = new JLabel();
+        obstacle.setBounds(200, 200, 100, 100);
+        obstacle.setBackground(Color.green);
+        obstacle.setOpaque(true);
+
+
         label = new JLabel();
         label.setBounds(0,0,100,100);
         label.setBackground(Color.red);
         label.setOpaque(true);
         frame.add(label);
+        frame.add(obstacle);
         frame.getContentPane().setBackground(Color.blue);
         frame.setLayout(null);
         frame.addKeyListener(this);
         frame.setVisible(true);
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        switch(e.getKeyChar()) {
-            case 'w':
-                label.setLocation(label.getX(), label.getY() - 10);
-                break;
-            case 'a':
-                label.setLocation(label.getX() - 10, label.getY());
-                break;
-            case 's':
-                label.setLocation(label.getX(), label.getY() + 10);
-                break;
-            case 'd':
-                label.setLocation(label.getX() + 10, label.getY());
-                break;
-        }
+
+    private boolean isCollision(int x, int y) {
+        Rectangle newBounds = new Rectangle(x, y, label.getWidth(), label.getHeight());
+        return !newBounds.intersects(obstacle.getBounds());
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
+
+        int x = label.getX();
+        int y = label.getY();
+        int step = 10;
+
         switch(e.getKeyCode()) {
-            case 'w':
-                label.setLocation(label.getX(), label.getY() - 10);
+            case KeyEvent.VK_W:
+                if (isCollision(x, y - step)) {
+                    System.out.println("You moved forwards!");
+                      label.setLocation(x, y - step);
+                }
                 break;
-            case 'a':
-                label.setLocation(label.getX() - 10, label.getY());
+            case KeyEvent.VK_A:
+                if (isCollision(x - step, y)) {
+                    label.setLocation(x - step, y);
+                }
                 break;
-            case 's':
-                label.setLocation(label.getX(), label.getY() + 10);
+            case KeyEvent.VK_S:
+                if (isCollision(x, y + step)) {
+                    label.setLocation(x, y + step);
+                }
                 break;
-            case 'd':
-                label.setLocation(label.getX() + 10, label.getY());
+            case KeyEvent.VK_D:
+                if (isCollision(x + step, y)) {
+                    label.setLocation(x + step, y);
+                }
                 break;
         }
     }
@@ -69,4 +81,13 @@ public class myFrame extends JFrame implements KeyListener {
             System.out.println("You moved forwards!");
         }
     }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // System.out.println("You Typed the Keychar:" + e.getKeyChar());
+        // System.out.println("You Typed the KeyCode:" + e.getKeyCode());
+    }
+
+
 }
+
